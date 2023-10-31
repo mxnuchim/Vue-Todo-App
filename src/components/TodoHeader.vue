5e8de157dc78dae02ded955883d05c0887c9d943
mxnuchim <manuchimoliver779@gmail.com>
2023-10-31 11:52:46 +0100

    feat: project complete

---
+++ src/components/TodoHeader.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <header>
+    <nav class="container">
+      <div class="branding">
+        <img src="@/assets/Todo_Logo.png" alt="" />
+        <h1>M's Todos</h1>
+      </div>
+      <ul class="nav-routes">
+        <RouterLink :to="{ name: 'todos' }">Todos</RouterLink>
+        <RouterLink :to="{ name: 'about' }">About</RouterLink>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+  background-color: #fff;
+  nav {
+    display: flex;
+    align-items: center;
+    padding: 25px 16px;
+
+    .branding {
+      display: flex;
+      align-items: center;
+      gap: 2px;
+      img {
+        max-width: 70px;
+      }
+
+      h1 {
+        font-size: 24px;
+      }
+    }
+
+    .nav-routes {
+      display: flex;
+      flex: 1;
+      justify-content: flex-end;
+      gap: 12px;
+      list-style: none;
+
+      a {
+        text-decoration: none;
+        color: inherit;
+      }
+    }
+  }
+}
+</style>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="about">
+    <h2>About the app</h2>
+    <p class="about-text">
+      Welcome to my personal task management app, I designed this to streamline
+      my daily life, and learn Vue lol. Whether you're a busy professional, a
+      student juggling assignments, or simply someone looking to boost
+      productivity, this app provides a user-friendly interface and powerful
+      features. Say goodbye to the chaos of scattered to-do lists and embrace a
+      more organized and focused life. Cheers🎉
+    </p>
+  </div>
+</template>
+
+<style>
+.about {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  padding-left: 8px;
+  padding-right: 8px;
+  gap: 15px;
+}
+
+.about-text {
+  font-size: 16px;
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    padding-left: 15%;
+    padding-right: 15%;
+  }
+  .about-text {
+    font-size: 18px;
+    text-align: center;
+  }
+}
+</style>
